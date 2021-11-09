99b7811418a7387ad05d280eb1513e703a96fc65
vuquang64 <nguyenvuquangE1@gmail.com>
2021-11-09 02:24:53 -0800

    update repo
    adding all the things needed

---
+++ app/src/main/java/com/example/cooklet_victor_work/PreparationActivity.java
@@ -0,0 +1,13 @@
+package com.example.cooklet_victor_work;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreparationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preparation);
+    }
+}
--- app/src/main/java/com/example/cooklet_victor_work/Step3Activity.java
+++ app/src/main/java/com/example/cooklet_victor_work/Step3Activity.java
@@ -1,13 +1,58 @@
 package com.example.cooklet_victor_work;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Step3Activity extends AppCompatActivity {
+
+    private Button timer;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMilliseconds = 18000;
+    private boolean timerRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step3);
+
+        timer = findViewById(R.id.timer);
+
+        timer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startStop();
+            }
+        });
+    }
+
+    public void startStop() {
+        if(timerRunning){
+            stopTimer();
+        } else {
+            startTimer();
+        }
+    }
+
+    private void startTimer() {
+        countDownTimer=new CountDownTimer(timeLeftInMilliseconds,1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeftInMilliseconds=l;
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+        timerRunning=true;
+    }
+    private void stopTimer() {
+        countDownTimer.cancel();
+        timerRunning=false;
     }
 }
