5d247f85521941eac16346a6628eeb07cded73f9
vuquang64 <nguyenvuquangE1@gmail.com>
2021-11-08 23:27:21 -0800

    creating

---
+++ app/src/main/java/com/example/cooklet_victor_work/Instruction.java
@@ -0,0 +1,141 @@
+package com.example.cooklet_victor_work;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Instruction extends AppCompatActivity {
+
+    LinearLayout Layout_bars;
+    TextView[] bottomBars;
+    int[] screens;
+    Button Home,Back;
+    ViewPager vp;
+    MyViewPagerAdapter myvpAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruction);
+        screens = new int[]{
+                R.layout.activity_step1,
+                R.layout.activity_step2,
+                R.layout.activity_step3,
+                R.layout.activity_step4
+        };
+        vp = (ViewPager) findViewById(R.id.view_pager);
+        Layout_bars = (LinearLayout) findViewById(R.id.layoutBars);
+        Back = (Button) findViewById(R.id.back);
+        Home = (Button) findViewById(R.id.home);
+        myvpAdapter = new MyViewPagerAdapter();
+        vp.setAdapter(myvpAdapter);
+        vp.addOnPageChangeListener(viewPagerPageChangeListener);
+        ColoredBars(0);
+    }
+
+    public void next(View v) {
+        int i = getItem(+1);
+        if (i < screens.length) {
+            vp.setCurrentItem(i);
+        } else {
+            launchMain();
+        }
+    }
+
+    public void skip(View view) {
+        launchMain();
+    }
+
+    private void ColoredBars(int thisScreen) {
+        int[] colorsInactive = getResources().getIntArray(R.array.dot_on_page_not_active);
+        int[] colorsActive = getResources().getIntArray(R.array.dot_on_page_active);
+        bottomBars = new TextView[screens.length];
+
+        Layout_bars.removeAllViews();
+        for (int i = 0; i < bottomBars.length; i++) {
+            bottomBars[i] = new TextView(this);
+            bottomBars[i].setTextSize(100);
+            bottomBars[i].setText(Html.fromHtml("¯"));
+            Layout_bars.addView(bottomBars[i]);
+            bottomBars[i].setTextColor(colorsInactive[thisScreen]);
+        }
+        if (bottomBars.length > 0)
+            bottomBars[thisScreen].setTextColor(colorsActive[thisScreen]);
+    }
+
+    private int getItem(int i) {
+        return vp.getCurrentItem() + i;
+    }
+
+    private void launchMain() {
+        startActivity(new Intent(Instruction.this, MainActivity.class));
+        finish();
+    }
+
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            ColoredBars(position);
+            if (position == screens.length - 1) {
+                Home.setText("Finish!");
+                Back.setVisibility(View.GONE);
+            } else {
+                Home.setText("Home");
+                Back.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater inflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(screens[position], container, false);
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return screens.length;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View v = (View) object;
+            container.removeView(v);
+        }
+
+        @Override
+        public boolean isViewFromObject(View v, Object object) {
+            return v == object;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cooklet_victor_work/Step2Activity.java
@@ -0,0 +1,13 @@
+package com.example.cooklet_victor_work;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Step2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step2);
+    }
+}
